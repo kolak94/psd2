f382dc37d9843e3a778d7f362fc00b54d92bfe37
Bartosz Wolak <bartosz.wolak94@gmail.com>
2018-02-12 20:12:42 +0100

    Complete

---
+++ smoothScroll.js
@@ -0,0 +1,18 @@
+// Select all links with hashes
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+				return false;
+			}
+		}
+});
